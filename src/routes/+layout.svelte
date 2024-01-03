6d8fd4319978dfa7f9a74546bda1243acf28baf4
jaredblack <jrdblck@gmail.com>
2024-01-02 17:27:15 -0800

    Initial project (Skeleton - sveltekit & tailwind)

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+</script>
+<AppShell>
+	<svelte:fragment slot="header">
+		
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Trivia Library</strong>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<slot />
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<div class="container">
+	<h1 class="h1 mx-auto p-8 space-y-8">Trivia Library</h1>
+</div>
